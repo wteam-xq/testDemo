9ef2229aa7a133a66d84e1e56301ba5f4db66b0a
wteam-xq <857609086@qq.com>
2015-08-09 23:25:57 +0800

    添加缓存demo代码（待解决问题1个）

--- utils.js
+++ utils.js
@@ -40,13 +40,13 @@ EventTarget.prototype = {
 var EventUtil = {
   // 添加事件
   addHandler: function(element, type, handler){
-      if (element.addEventListener){
-          element.addEventListener(type, handler, false);
-      }else if (element.attachEvent){
-          element.attachEvent("on" + type, handler);
-      }else{
-          element["on" + type] = handler;
-      }
+    if (element.addEventListener){
+        element.addEventListener(type, handler, false);
+    }else if (element.attachEvent){
+        element.attachEvent("on" + type, handler);
+    }else{
+        element["on" + type] = handler;
+    }
   },
   getEvent: function(event){
     return event ? event : window.event;
@@ -79,6 +79,45 @@ var EventUtil = {
     }else{
         event.cancelBubble = true;
     }
+  }
+};
+
+// cookies管理类
+var CookieUtil = {
+  get: function (name){
+    var cookie_name = encodeURIComponent(name) + "=",
+    cookie_start = document.cookie.indexOf(cookie_name),
+    cookie_value = null;
+
+    if (cookie_start > -1) {
+      var cookie_end = document.cookie.indexOf(";", cookie_start);
+      if (cookie_end == -1) {
+        cookie_end = document.cookie.length;
+      }
+      cookie_value = decodeURIComponent(document.cookie.substring(cookie_start + cookie_name.length, cookie_end));
+    }
+    return cookie_value;
+  },
+  set: function(name, value, expires, path, domain, secure) {
+    var cookie_text = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    if (expires instanceof Date) {
+      cookie_text += "; expires=" + expires.toGMTString(); 
+    } else{
+      cookie_text += "; expires=" + new Date().toGMTString(); 
+    }
+    if (path) {
+      cookie_text += "; path=" + path;
+    }
+    if (domain) {
+      cookie_text += "; domain=" + domain;
+    }
+    if (secure) {
+      cookie_text += "; secure";
+    }
+    document.cookie = cookie_text;
+  },
+  unset: function (name, path, domain, secure) {
+    this.set(name, "", new Date(0), path, domain, secure);
   }
 };
 
