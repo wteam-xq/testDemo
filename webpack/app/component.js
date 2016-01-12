ab16c0b2fe07768f1480c91480619df7745fbf6c
wteam-xq <zhiqiangxiao@sohu-inc.com>
2016-01-12 13:12:29 +0800

    webpack 测试小改动

---
+++ webpack/app/component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function () {
+    var element = document.createElement('h1');
+    element.innerHTML = 'Hello world';
+    return element;
+};
---
+++ webpack/app/main.js
@@ -0,0 +1,4 @@
+'use strict';
+var component = require('./component.js');
+
+document.body.appendChild(component());
