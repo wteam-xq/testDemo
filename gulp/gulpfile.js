6a39d634935809cddd0febae4d4178f51819a577
wteam-xq <857609086@qq.com>
2016-02-21 11:59:25 +0800

    gulp新增本地压缩js css html配置

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -8,8 +8,9 @@ https://github.com/nimojs/gulp-book
 var gulp = require('gulp'),
     // js 压缩插件 （用于压缩 JS）
     uglify = require('gulp-uglify'),
-    // 压缩css插件
+    // 压缩css插件(cssnano将取代gulp-minify-css)
     minifyCSS = require('gulp-minify-css'),
+    cssnano = require('gulp-cssnano'),
     // 获取 gulp-imagemin 模块
     imagemin = require('gulp-imagemin'),
     // 重命名 插件
@@ -160,11 +161,36 @@ gulp.task('default', ['clean'], function(){
 });
 
 /*************************************************************
- *                本地js  html css字符串压缩      
+ *               本地js  html css本地压缩      
  ************************************************************/
- // 字符串拷贝进 js/str.js 中, 然后运行 `gulp str-js`
-
- // 字符串拷贝进 css/str.css 中, 然后运行 `gulp str-css`
-
- // 字符串拷贝进 str.html 中, 然后运行 `gulp str-html`
-
+// 字符串拷贝进 js/str.js 中, 然后运行 `gulp str-js`
+gulp.task('str-js', function() {
+    gulp.src('js/str.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+// 字符串拷贝进 css/str.css 中, 然后运行 `gulp str-css`
+gulp.task('str-css', function () {
+    gulp.src('css/str.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
+});
+// 字符串拷贝进 str.html 中, 然后运行 `gulp str-html`
+gulp.task('str-html', function () {
+    var options = {
+        collapseWhitespace: true,//压缩HTML
+        //省略布尔属性的值 <input checked="true"/> ==> <input />
+        collapseBooleanAttributes: false,
+        //删除所有空格作属性值 <input id="" /> ==> <input />
+        removeEmptyAttributes: true,
+        //删除<script>的type="text/javascript"
+        removeScriptTypeAttributes: true,
+        //删除<style>和<link>的type="text/css"
+        removeStyleLinkTypeAttributes: true,
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('str.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist'));
+});
---
+++ gulp/js/str.js
@@ -0,0 +1,17 @@
+// gulp压缩js，此js文件必须没有语法错误；（所以不能将json格式，需要用括号包含起来）
+'{
+  "array": [
+    1,
+    2,
+    3
+  ],
+  "boolean": true,
+  "null": null,
+  "number": 123,
+  "object": {
+    "a": "b",
+    "c": "d",
+    "e": "f"
+  },
+  "string": "Hello World"
+}'
