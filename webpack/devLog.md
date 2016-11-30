e53569e625c57a0b8c39a1f726c40f79d2ff7863
zhiqiangxiao <zhiqiangxiao@sohu-inc.com>
2016-11-30 21:38:48 +0800

    webpack小demo更新

--- webpack/devLog.md
+++ webpack/devLog.md
@@ -1,9 +1,18 @@
 ### 学习日志
+#### 2016-11-30
+    * [webpack实现雪碧图功能](http://www.tuicool.com/articles/YZfeeu7)
+    * [webpack中文指南](http://webpackdoc.com/)
+    * 完成 webpack 生成雪碧图demo
 
-* 2016-1-5 webpack 安装全局中， 这样才能直接命令行调用`webpack`
-* 2016-1-10 使用webpack开发都得编译后才能调试代码？（依赖关系如何实现）
-* 2016-1-12 本地工程不需要安装webpack
-* 2016-01-13 webpack 引入前端项目可行性考虑；
+
+
+
+
+
+
+
+
+
 
 
 #### 理想工作流程
@@ -11,4 +20,4 @@
 2.上线前能针对功能模块 代码进行压缩；（gulp）
 3.能清除线上的缓存文件;(时间戳 gulp-rev配合gulp-rev-collector 引入文件添加版本号)
 4.线上能调试， 在地址栏加入对应的参数可以查看到没压缩前的代码；（研究中）
-5.很方便上线（github中转, 本地代码提交到github, 服务器对应工程git pull）；
+5.很方便上线（github中转, 本地代码提交到github, 服务器对应工程git pull）: git自动化部署；
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,28 @@
 var path = require('path');
+var SpritesmithPlugin = require('webpack-spritesmith');
 
 module.exports = {
     entry: path.resolve(__dirname, 'app/main.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
-    }
+    },
+    plugins: [
+        new SpritesmithPlugin({
+          src: {
+            cwd: path.resolve(__dirname, 'app/images/'),
+            glob: '*.png'
+          },
+          target: {
+            image: path.resolve(__dirname, 'build/images/sprite.png'),
+            css: path.resolve(__dirname, 'build/css/sprite.css')
+          },
+          apiOptions: {
+            cssImageRef: '../images/sprite.png'
+          },
+          spritesmithOptions: {
+            algorithm: 'top-down'
+          }
+        })
+    ]
 };
